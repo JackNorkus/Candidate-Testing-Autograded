e236853b35517c82f25ab700d5a7607666e8ae0d
JackNorkus <norkusjr@gmail.com>
2024-01-15 19:14:40 -0600

    finished assignment 1 part 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,13 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+
+let candidateName = ""; // DONE: defined candidateName as the empty string "" //
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = ""; // DONE: all three variables initialized //
 
 
 //TODO: Variables for Part 2
@@ -19,18 +21,33 @@ let candidateAnswers;
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
 
+  candidateName = input.question("What is your name? "); // DONE: defined the function askForName(), //
+                                                         // which asks the question "What is your    //
+                                                         // name?" and stores the user input into    //
+                                                         // the variable candidateName               //
+
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
+  candidateAnswer = input.question(question); // DONE: defined the function askQuestion(), which     //
+                                              // asks the question stored in the variable question,  //
+                                              // and stores the user input into the variable         //
+                                              // candidateAnswer                                     //
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  if (correctAnswer.toLowerCase === candidateAnswer.toLowerCase) {
+    console.log("Correct!");
+  }
 
+  else {
+    console.log("Sorry, that is incorrect.");
+  } // DONE: used a conditional statement to decide whether the candidate's answer was correct //
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
@@ -42,7 +59,11 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log();
+
+   console.log("Hello, " + candidateName + "!"); // DONE: wrote a console.log statement, which   //
+                                                 // prints a concatenated string to the terminal //
+                                                 // greeting the user by their name              //
+
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
