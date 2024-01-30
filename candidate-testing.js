93990d135ea61c1dcfcd80dad38b3e4e52d76380
JackNorkus <norkusjr@gmail.com>
2024-01-29 20:18:59 -0600

    this is for git actions

--- candidate-testing.js
+++ candidate-testing.js
@@ -2,7 +2,7 @@ const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
 
-// TODO 1.1a: Define candidateName // 
+// TODO 1.1a: Define candidateName //   
 
 let candidateName = ""; // DONE: defined candidateName as the empty string "" //
 
